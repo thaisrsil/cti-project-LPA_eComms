9afd8d56537227dbe797523330205440e1e4eed4
thaisrsil <80487110+thaisrsil@users.noreply.github.com>
2024-10-14 21:16:50 +1000

    PHP added
    add php

---
+++ header.php
@@ -0,0 +1,22 @@
+<!-- header.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="images/books.ico" type="image/x-icon">
+    <title>Books For You</title>
+</head>
+<body>
+    <header>
+        <img src="images/books-icon-small.png" align="left" alt="Books Icon">
+        <h1>Books For You</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php?page=home">Home</a></li>
+                <li><a href="index.php?page=horror">Horror</a></li>
+                <li><a href="index.php?page=afterpay">After Pay</a></li>
+            </ul>
+        </nav>
+    </header>
